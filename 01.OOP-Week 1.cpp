c059d9eca67b81147daa3108cdc44936c496cadc
Vankata <vancyki@abv.bg>
2016-03-13 21:46:51 +0200

    OOP Week 1

---
+++ 01.OOP-Week 1.cpp
@@ -0,0 +1,29 @@
+﻿// labyrinth.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+using namespace std;
+
+struct Student
+{
+	char name[100];
+	int fn;
+	double age;
+};
+
+int main()
+{
+	
+	Student Pesho;
+	Pesho.age = 21;
+	Pesho.fn = 71629;
+	int x;
+	cin >> x;
+	char* darr = new char[x];
+	delete []darr;
+
+	
+
+	return 0;
+}
